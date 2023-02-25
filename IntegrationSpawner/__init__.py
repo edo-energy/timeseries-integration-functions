bf381b0e2c41b29db6fa7fdc368bd74b74438fa4
Adam Hilton <adamh@edoenergy.com>
2023-02-24 18:17:22 -0700

    Add integration calling trigger.

---
+++ IntegrationSpawner/__init__.py
@@ -0,0 +1,25 @@
+import datetime
+import logging
+
+import azure.functions as func
+import azure.durable_functions as df
+
+integrations = [
+    "WeatherCollection"
+]
+
+
+async def main(mytimer: func.TimerRequest, starter: str) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    logging.info(f"Starting to spawn daily integrations at {utc_timestamp}")
+
+    client = df.DurableOrchestrationClient(starter)
+
+    for integration in integrations:
+        logging.info(f"Sending due integration {integration}")
+
+        instance_id = await client.start_new(integration)
+
+        logging.info(f"Started integration with ID '{instance_id}'.")
